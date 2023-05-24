fd87e7628824ad1220e9fad85292e759015f2750
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2023-05-24 12:06:18 -0700

    MODEL AJUSTMENT:

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,7 +6,7 @@ namespace API.Models
         public string Name { get; set; }
         public DateTime Release_Year { get; set; }
         public string Gender { get; set; }
-        public TimeSpan Duration { get; set; }
+        public string Duration { get; set; }
         public int Director { get; set; }
     }
 }
